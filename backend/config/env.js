362359adcffe96deb910d9d7a84d322b0d840992
Hercrat <l21070074@gmail.com>
2025-03-19 22:49:59 -0600

    funcionando

--- backend/config/env.js
+++ backend/config/env.js
@@ -1,6 +1,6 @@
-function env_() {
-    return{
-        ID_DATABASE:"",
-        SH_REGISTRO_USUARIOS:"",
-    };
-}
+// function env_() {
+//     return{
+//         ID_DATABASE:"1a_IZHJezqBp6b2xbqBb_UAeX6RAGi3OT",
+//         SH_REGISTRO_USUARIOS:"Registro_Asistencia",
+//     };
+// }
